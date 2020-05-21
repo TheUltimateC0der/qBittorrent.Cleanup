66c688b93621d8cb49e3945f8e6cb3d5617ccb4c
UltimateC0der <no@e.mail>
2020-05-21 02:47:01 +0200

    Add project files.

---
+++ qBittorrent.Cleanup/Options.cs
@@ -0,0 +1,29 @@
+﻿
+using CommandLine;
+
+namespace qBittorrent.Cleanup
+{
+    public class Options
+    {
+
+        [Option('u', "user", Required = true, HelpText = "Username for your qBittorrent instance")]
+        public string Username { get; set; }
+
+
+        [Option('p', "pass", Required = true, HelpText = "Password for your qBittorrent instance")]
+        public string Password { get; set; }
+
+
+        [Option("url", Required = true, HelpText = "Url for your qBittorrent instance")]
+        public string Url { get; set; }
+
+
+        [Option("path", Required = true, HelpText = "The path to the directory containing your torrents")]
+        public string Path { get; set; }
+
+
+        [Option("dry-run", Required = false, HelpText = "Just outputs the things it would do, without actually doing it")]
+        public bool DryRun { get; set; }
+
+    }
+}
---
+++ qBittorrent.Cleanup/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using CommandLine;
+
+using QBittorrent.Client;
+
+namespace qBittorrent.Cleanup
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var parsedArguments = Parser.Default.ParseArguments<Options>(args);
+
+            await parsedArguments.WithParsedAsync(RunOptions);
+        }
+
+
+        private static async Task RunOptions(Options opts)
+        {
+            if (opts.DryRun)
+                Console.WriteLine($"Dry run!");
+
+            var deleteCount = 0;
+            long spaceSaved = 0;
+
+            var client = new QBittorrentClient(new Uri(opts.Url));
+            await client.LoginAsync(opts.Username, opts.Password);
+
+            var torrents = await client.GetTorrentListAsync();
+
+            foreach (var directory in Directory.GetDirectories(opts.Path).OrderBy(x => x))
+            {
+                var dirName = Path.GetFileName(directory);
+                if (!torrents.Any(x => x.Name == dirName))
+                {
+                    var dirSize = DirSize(new DirectoryInfo(directory));
+
+                    Console.WriteLine($"Deleting: ({BytesToString(dirSize)}) {directory}");
+
+                    if (!opts.DryRun)
+                        Directory.Delete(directory, true);
+
+                    deleteCount++;
+                    spaceSaved += dirSize;
+                }
+            }
+
+            Console.WriteLine($"Deleted {deleteCount} of left behind torrents which saves {BytesToString(spaceSaved)}.");
+        }
+
+
+        private static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            // Add file sizes.
+            var fis = d.GetFiles();
+            foreach (var fi in fis)
+            {
+                size += fi.Length;
+            }
+            // Add subdirectory sizes.
+            var dis = d.GetDirectories();
+            foreach (var di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+
+        static string BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+                return "0" + suf[0];
+            var bytes = Math.Abs(byteCount);
+            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+    }
+}
